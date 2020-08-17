a721d9e845297c4d033d5b31a780cbfe5d8003fb
JuanDuran85 <duran85juan@gmail.com>
2020-08-17 19:52:32 -0400

    trabajando con servidor en express

--- ejemplo2/index.js
+++ ejemplo2/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send("mensaje desde servidor con express...");
+app.get('/mensaje',(req,res)=>{
+    res.send(`mensaje desde servidor con express... El mensaje recibido es: ${req.query.mensaje}`);
 });
+
+app.post('/',(req,res)=>{
+    res.send(`Mensaje enviado: ${req.body.texto}`)
+})
 
 app.listen(8080);
 console.log("Ejecutando el servidor");
