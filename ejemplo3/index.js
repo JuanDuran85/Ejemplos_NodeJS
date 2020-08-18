05e68e8e4e24b1b3572d02d07960674892686396
JuanDuran85 <duran85juan@gmail.com>
2020-08-18 17:53:54 -0400

    trabajando con servidor en express y archivos estaticos

--- ejemplo3/index.js
+++ ejemplo3/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
 let app = express();
+app.set('view engine','ejs');
+
 //app.use('/assets',express.static('assets'));
 //app.use('/assets', express.static(__dirname + '/assets')); // otra manera...
-app.use('/assets', express.static(`${__dirname}/assets/`)); // otra forma
+app.use('/assets', express.static(`${__dirname}/assets/`,{
+    etag: false,
+    maxAge: 60000,
+})); // otra forma
 
-app.get('/',(req,res)=>{
+/* curl http://localhost:8080/assets/style.css -v */
+
+
+/* app.get('/',(req,res)=>{
     res.sendFile('index.html',{
         root: __dirname})
+}); */
+
+app.get('/',(req, res)=>{
+    res.render('index');
 });
 
 app.listen(8080);
