ffaa8647292c7c446cd27116dcddb5f624f7f8c6
JuanDuran85 <duran85juan@gmail.com>
2020-08-21 14:03:21 -0400

    trabajando con cookies

--- ejemplo4/index.js
+++ ejemplo4/index.js
@@ -8,6 +8,11 @@ app.use(cookieSession({
     keys: ['5cf81845814ad6696a182af24cbe6e6d','f3b1e5e806faa1a0ad49e48b0c9946b8']
 })); // se debe enviar un objeto json con la configuracion
 
-
+app.use('/',(req,res)=>{
+    req.session.views = (req.session.views || 0) + 1;
+    req.session.visit = (req.session.visit || 0) + 1;
+    res.send(`Visitando el servidor: ${req.session.visit} visita(s). Viendo el servidor: ${req.session.views}`)
+})
 
 app.listen(8080);
+console.log("Servidor activo");
