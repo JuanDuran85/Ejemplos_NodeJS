d2c7c4ca14201806542fbbc4e452695b407f7397
JuanDuran85 <duran85juan@gmail.com>
2020-08-23 22:01:10 -0400

    agregando nuevas rutas y vistas

--- ejemplo6/controllers/tarreas.js
+++ ejemplo6/controllers/tarreas.js
@@ -1,6 +1,19 @@
 const Tarrea = require('../models').Tarrea;
 
 module.exports = {
+    create: function(req,res){
+        Tarrea.create({
+            descripcion: req.body.descripcion
+        }).then(resultado=>{
+            res.json(resultado);
+        }).catch(err=>{
+            console.log(err);
+            res.json(err);
+        })
+    },
+    nueva: function (req,res){
+        res.render('tarreas/nueva');
+    },
     home: function (req,res) {
         Tarrea.findAll().then((datos)=>{
             res.render('tarreas/index',{
--- ejemplo6/routes/tareas_rutas.js
+++ ejemplo6/routes/tareas_rutas.js
@@ -1,11 +1,13 @@
 const express = require('express');
+let TarreasControlador = require('../controllers/tarreas');
 const router = express.Router();
 
 router.route('/tareas').get(function (req,res){
     console.log('desde la ruta tareas...');
-    res.send("mensaje desde la ruta tareas con get...");
-}).post(function (req,res){
-    console.log("En la ruta tareas con post");
-});
+    //res.send("mensaje desde la ruta tareas con get...");
+}).post(TarreasControlador.create);
+
+router.get('/tareas/nueva',TarreasControlador.nueva);
+router.get('/tareas/index',TarreasControlador.home);
 
 module.exports = router;
