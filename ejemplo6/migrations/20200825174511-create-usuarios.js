a10359e204c1dc1d849fd0f10878196b14eb598b
JuanDuran85 <duran85juan@gmail.com>
2020-08-25 14:04:05 -0400

    agregando modelo y migracion de usuarios

---
+++ ejemplo6/migrations/20200825174511-create-usuarios.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true, // dato unico, que no se repita
+        allowNull: false //cualquier dato que no deje vacia la tabla
+      },
+      passwod_hash: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
