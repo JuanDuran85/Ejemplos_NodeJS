909aa0cfb064993b23e2e7ef664b02a8ab43866b
JuanDuran85 <duran85juan@gmail.com>
2020-12-17 00:38:30 -0300

    agregando rutas para buscar

--- ejemplo8/index.js
+++ ejemplo8/index.js
@@ -5,7 +5,7 @@ let pieRepo = require('./repos/pieRepo');
 let router = express.Router(); // rutas de express para dejar activa
 /* let pies = pieRepo.get(); */
 
-router.get('/',(req,res,next)=>{ // creando get para retornar un valor
+router.get('/',function(req,res,next){ // creando get para retornar un valor
 /*     res.status(200).send(pies); */
     pieRepo.get(function(data){
         res.status(200).json({
@@ -17,6 +17,48 @@ router.get('/',(req,res,next)=>{ // creando get para retornar un valor
     }, function(err){
         next(err);
     })
+});
+
+router.get('/search',function (req,res,next) {  
+    let searchObject = {
+        "id": req.query.id,
+        "name": req.query.name
+    };
+    pieRepo.search(searchObject,function (data) {  
+        res.status(200).json({
+            "status": 200,
+            "statusText": "Ok",
+            "message":"All data retrieved",
+            "data": data 
+        })
+    }, function (err) { 
+        next(err) 
+    });
+})
+
+router.get('/:id',function (req,res,next){
+    pieRepo.getById(req.params.id,function (data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "Ok",
+                "message":"Single data retrieved",
+                "data": data 
+            })
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message":"The data '"+req.params.id+"' could not be found",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The data '"+req.params.id+"' could not be found"
+                }
+            })
+        }
+    },function(err){
+        next(err)
+    });
 });
 
 app.use('/api/', router); // agregando ruta
--- ejemplo8/repos/pieRepo.js
+++ ejemplo8/repos/pieRepo.js
@@ -10,6 +10,32 @@ let pieRepo = {
                 resolve(JSON.parse(data));
             }
         });
+    },
+    getById: function (id,resolve,reject) {
+        fs.readFile(FILE_NAME, function (err,data){
+            if(err){
+                reject(err);
+            } else {
+                let pie = JSON.parse(data).find(result => result.id == id);
+                resolve(pie);
+            }
+        });
+    },
+    search: function(searchObject,resolve,reject){
+        fs.readFile(FILE_NAME, function(err,data){
+            if (err){
+                reject(err);
+            } else {
+                let pies = JSON.parse(data);
+                if (searchObject){
+                    pies = pies.filter(
+                        p => 
+                            (searchObject.id ? p.id == searchObject.id : true) && (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true)
+                    );
+                }
+                resolve(pies);
+            }
+        })
     }
 };
 
