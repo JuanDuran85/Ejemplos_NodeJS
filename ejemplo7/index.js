67fe82d9857fb1e2c7da1bf5756a70040aa4b8af
JuanDuran85 <duran85juan@gmail.com>
2021-01-24 20:55:18 -0300

    ultimos cambios

--- ejemplo7/index.js
+++ ejemplo7/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions'); 
-const admin = require('firebase-admin'); 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase); 
 const express = require('express'); 
 const cors = require('cors');
