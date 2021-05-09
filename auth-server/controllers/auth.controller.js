86a339117a1a17e5c9e858deb17d35f24c2fda1f
JuanDuran85 <duran85juan@gmail.com>
2021-05-08 22:46:54 -0400

    realizando middlewares personalizados

--- auth-server/controllers/auth.controller.js
+++ auth-server/controllers/auth.controller.js
@@ -2,8 +2,9 @@ const { response } = require('express');
 
 //controlador para crear usuario
 const createUser = (req,res = response) => {
+
     const { name, email, password } = req.body;
-    console.log(name, email, password);
+
     return res.json({
         ok:true,
         msg:"crear usuario /new"
@@ -12,8 +13,9 @@ const createUser = (req,res = response) => {
 
 //controlador para login de usuario
 const loginUser = (req,res = response) => {
+
     const { email, password } = req.body;
-    console.log(email, password);
+
     return res.json({
         ok:true,
         msg:"login de usuario /"
--- auth-server/routes/auth.route.js
+++ auth-server/routes/auth.route.js
@@ -1,15 +1,26 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { createUser, loginUser, tokenValid } = require('../controllers/auth.controller');
+const { validFields } = require('../middlewares/validar-campos.middleware');
 
 const router = Router();
 
 /* definiendo rutas */
 
 //crear un nuevo usuario
-router.post('/new', createUser);
+router.post('/new', [
+    check('email','El email es obligatorio').isEmail().normalizeEmail(),
+    check('password','La contraseña es obligatoria').isLength({min:6}).withMessage('La contraseña debe contener al menos 6 caracteres').matches(/\d/).withMessage('La contraseña debe contener al menos un numero').not().isIn(['123456', 'password']).withMessage('No utilices palabras basicas como 123456'),
+    check('name','El nombre es obligatorio').not().isEmpty().isLength({min:2}).withMessage('El nombre debe llevar al menos dos caracteres').not().matches(/\d/).withMessage('El nombre no puede llevar numeros'),
+    validFields
+] ,createUser);
 
 //login de usuario
-router.post('/', loginUser);
+router.post('/', [
+    check('email','El email es obligatorio').isEmail().normalizeEmail(),
+    check('password','La contraseña es obligatoria').isLength({min:6}).withMessage('La contraseña debe contener al menos 6 caracteres').matches(/\d/).withMessage('La contraseña debe contener al menos un numero').not().isIn(['123456', 'password']).withMessage('No utilices palabras basicas como 123456'),
+    validFields
+], loginUser);
 
 //validar y revalidar token
 router.get('/renew', tokenValid);
