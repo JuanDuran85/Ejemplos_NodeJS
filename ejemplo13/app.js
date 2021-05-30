ae7c0cbe2d66d6069df2633ea1ca7e42116996dc
JuanDuran85 <duran85juan@gmail.com>
2021-05-30 00:08:52 -0400

    trabajando con librerias externas para manejar la consola

--- ejemplo13/app.js
+++ ejemplo13/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { getTablaMultiply } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
 
-getTablaMultiply(2)
+getTablaMultiply(argv.b, argv.l, argv.h)
     .then(resp => console.log(resp))
     .catch(error => console.log(error));
--- ejemplo13/helpers/multiplicar.js
+++ ejemplo13/helpers/multiplicar.js
@@ -1,22 +1,27 @@
 const fs = require('fs');
+const colors = require('colors');
 
-const getTablaMultiply = async (base) => {
+const getTablaMultiply = async (base = 1, lista = false, hasta = 10) => {
     try {
         console.clear();
         
-        let salida = '';
+        let salida = ''; 
+        let consola = '';
         
-        console.log("------------------------");
-        console.log(`       Table del ${base}      `);
-        console.log("------------------------");
-        
-        for (let i = 0; i <= 10; i++) {
+        console.log("------------------------".green);
+        console.log(`       Table del ${base}      `.underline.red);
+        console.log("------------------------".green);
+
+        for (let i = 0; i <= hasta; i++) {
             salida += `${base} * ${i} = ${base*i} \n`;
+            consola += `${base} ${'*'.yellow} ${i} ${'='.yellow} ${base*i} \n`;
         };
         
-        console.log(salida);
+        if (lista) {
+            console.log(consola);
+        }
         
-        fs.writeFileSync(`tabla-${base}.txt`,salida);
+        fs.writeFileSync(`./out/tabla-${base}.txt`,salida);
     
         return `tabla-${base}.txt`;
     } catch (error) {
