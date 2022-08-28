fddea492b6098d10e3900f7047e9927e6e560b61
Juan <duran85juan@gmail.com>
2022-08-28 18:12:02 -0400

    test with vitest

--- test-node/basic-testing-frontend/test/numbers.test.js
+++ test-node/basic-testing-frontend/test/numbers.test.js
@@ -1,8 +1,8 @@
 
 import { describe,it, expect } from 'vitest';
-import { transformToNumber } from '../src/util/numbers';
+import { transformToNumber, cleanNumbers } from '../src/util/numbers';
 
-describe('Test to Numbers function',()=>{
+describe('Test to transformToNumber function',()=>{
     it('Should be able to transform a number into a number',()=>{
         // Arange
         const num = 4;
@@ -43,4 +43,28 @@ describe('Test to Numbers function',()=>{
         expect(resultTwo).toBeNaN();
         expect(resultThree).toBeNaN();
     })
+})
+
+describe('Testo to cleanNumbers function',()=>{
+    it('should return an array of number values if an array of string number values is provider', () => {
+        // Arange
+        const arrayString = ['1','2','3'];
+
+        // Act
+        const result = cleanNumbers(arrayString);
+
+        // Assert
+        expect(result[0]).toBeTypeOf('number')
+    });
+
+    it('should throw an error if an array with at least one empty string is provided', () => {
+        // Arange
+        const arrayString = ['','3'];
+        
+        // Act
+        const result = () => cleanNumbers(arrayString);
+
+        // Assert
+        expect(result).toThrow();
+    });
 })
