326ad05e8ed120d028872f39772b253158d08a4f
Juan <duran85juan@gmail.com>
2022-09-15 23:18:17 -0300

    test with vitest

--- test-node/01-starting-project/test/io.test.js
+++ test-node/01-starting-project/test/io.test.js
@@ -3,9 +3,18 @@ import { promises as fs } from 'fs'
 import writeData from '../src/util/io';
 
 vi.mock('fs');
+vi.mock('path',()=>{
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1];
+            }
+        }
+    }
+});
 
 describe('Test to writeData function', () => {
-    it('Should execute the writeData method', () => {
+    it('Should execute the writeData method with two arguments', () => {
         // Arange
         const testData = "Test data string";
         const testFileName = "name_file.txt";
@@ -13,7 +22,31 @@ describe('Test to writeData function', () => {
         // Act
         writeData(testData, testFileName);
 
-        // Assert - return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+        // Assert
+        expect(fs.writeFile).toBeCalledWith(testFileName, testData);
+    });
+
+    it('Should return a promise that resolves to no value if called correctly', () => {
+        // Arange
+        const testData = "Test data string";
+        const testFileName = "name_file.txt";
+
+        // Act
+        writeData(testData, testFileName);
+
+        // Assert
+        return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+    });
+
+    it('Should to be called the writeData method', () => {
+        // Arange
+        const testData = "Test data string";
+        const testFileName = "name_file.txt";
+
+        // Act
+        writeData(testData, testFileName);
+
+        // Assert
         expect(fs.writeFile).toBeCalled();
     });
 });
