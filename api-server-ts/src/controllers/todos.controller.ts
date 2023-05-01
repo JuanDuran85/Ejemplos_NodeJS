9c361eaad60396c2ecd048ed18594c448cf68933
Juan <duran85juan@gmail.com>
2023-05-01 13:57:13 -0400

    working with typescript node express

--- api-server-ts/src/controllers/todos.controller.ts
+++ api-server-ts/src/controllers/todos.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 
 import { Todo } from "../models/todos.model";
 
@@ -12,5 +12,34 @@ export const createTodo = (req: Request, res: Response, next: NextFunction) => {
   res.status(201).json({
     message: "Created the Todo",
     createdTodo: newTodo,
+  });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(201).json({
+    todos: todosElements,
+  });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId: string = req.params?.id;
+  const todoIndex = todosElements.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) throw new Error("Could not find todo");
+  todosElements.splice(todoIndex, 1);
+  res.status(201).json({
+    messaje: "delete success",
+    updateTodo: todosElements,
+  });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId: string = req.params?.id;
+  const updateText: string = (req.body as { text: string })?.text;
+  const todoIndex = todosElements.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) throw new Error("Could not find todo");
+  todosElements[todoIndex] = new Todo(todosElements[todoIndex].id, updateText);
+  res.status(201).json({
+    messaje: "update success",
+    updateTodo: todosElements[todoIndex],
   });
 };
