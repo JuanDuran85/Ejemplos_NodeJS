909411f0c780795227559d7664f8b04080adae37
JuanDuran85 <duran85juan@gmail.com>
2025-11-07 23:30:36 -0300

    Add setTimeout examples to demonstrate asynchronous execution in app3.js

--- 01-fundamentals/app3.js
+++ 01-fundamentals/app3.js
@@ -1,1 +1,15 @@
-console.debug();
+console.debug('Start app3.js...');
+
+setTimeout(() => {
+    console.debug('Timeout 01...');
+}, 4000);
+
+setTimeout(() => {
+    console.debug('Timeout 02...');
+}, 1);
+
+setTimeout(() => {
+    console.debug('Timeout 03...');
+}, 0);
+
+console.debug('End app3.js...');
