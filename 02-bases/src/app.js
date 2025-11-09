abfb9907766a26efca67fa305dd8c8d47a8c3535
JuanDuran85 <duran85juan@gmail.com>
2025-11-09 16:51:34 -0300

    Add callback pattern implementation with user lookup functionality

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,8 +1,18 @@
 const { emailTemplate } = require("./js-foundations/01-template");
 require("./js-foundations/02-destructuring");
+const { getUserById } = require("./js-foundations/03-callbacks.js");
 
 console.debug("-----------------------");
 
 console.debug(emailTemplate);
+const id = 3;
+getUserById(id, function (error, user) {
+  if (error) {
+    console.debug(error);
+    throw new Error(error);
+  }
+
+  console.debug({ user });
+});
 
 console.debug("----------------------");
--- 02-bases/src/js-foundations/02-destructuring.js
+++ 02-bases/src/js-foundations/02-destructuring.js
@@ -1,6 +1,5 @@
-console.debug(process.env);
 console.debug(process.env.PORT ?? 3000);
 
 const { PUBLIC, ProgramData, USERNAME } = process.env;
 
-console.table({PUBLIC, ProgramData, USERNAME});
+console.table({ PUBLIC, ProgramData, USERNAME });
