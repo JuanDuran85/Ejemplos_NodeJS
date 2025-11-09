ac1903d2e8d2887a3650a8eb90616f3f42718ca3
JuanDuran85 <duran85juan@gmail.com>
2025-11-08 22:43:59 -0300

    Add nodemon dev dependency and update dev script

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,7 +1,7 @@
 const { emailTemplate } = require('./js-foundations/01-template');
 
-console.debug("---------------------------");
+console.debug("-----------------------");
 
 console.debug(emailTemplate);
 
-console.debug("---------------------------");
+console.debug("----------------------");
