dcfdd97fe91a542dda1971f76d510e80109b2603
JuanDuran85 <duran85juan@gmail.com>
2025-11-12 14:48:58 -0300

    Add TypeScript types and improve test assertions for getUserById callback tests

--- 02-bases/tests/js-fiundations/03-callbacks.test.ts
+++ 02-bases/tests/js-fiundations/03-callbacks.test.ts
@@ -3,17 +3,25 @@ import { describe, test, expect } from "@jest/globals";
 import { getUserById } from "../../src/js-foundations/03-callbacks";
 describe("App", () => {
   test("should get user by id ", () => {
-    const user = getUserById(1, (error, user) => {
+    const id: number = 1;
+    getUserById(id, (error, user) => {
       expect(user).toBeDefined();
+      expect(user).toMatchObject({
+        id: 1,
+        name: "Leanne Graham",
+      });
+      expect(user?.name).toContain("Leanne Graham");
+      expect(user?.name).toEqual("Leanne Graham");
+      expect(error).toBeUndefined();
     });
-    console.debug(user);
   });
 
-  test('Should get an error',() =>{
-    const user = getUserById(23, (error, user) => {
+  test("Should get an error", () => {
+    const id: number = 23;
+    getUserById(id, (error, user) => {
+      expect(user).toBeUndefined();
       expect(error).toBeDefined();
       expect(error).toBe("User not found with id 23");
     });
-    console.debug(user);
-  })
+  });
 });
