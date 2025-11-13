ee18d34787392d27f3b049eb8695b3a1d3dcfa72
JuanDuran85 <duran85juan@gmail.com>
2025-11-13 19:45:31 -0300

    feat(terminal): enhance yargs integration with detailed options for multiplication table

--- 04-terminal-app/src/app.ts
+++ 04-terminal-app/src/app.ts
@@ -2,11 +2,15 @@ import { yarg } from "./config/plugins/yargs.plugins";
 
 (async () => {
   console.debug("Start app...");
-  console.debug(yarg);
   await main();
   console.debug("End app...");
 })();
 
 async function main() {
+  console.debug(yarg);
+  console.debug(yarg.base);
+  console.debug(yarg.b);
+  const { b } = yarg;
+  console.debug(b);
   console.debug("Executed Code");
 }
--- 04-terminal-app/src/config/plugins/yargs.plugins.ts
+++ 04-terminal-app/src/config/plugins/yargs.plugins.ts
@@ -1,4 +1,35 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-export const yarg = yargs(hideBin(process.argv)).parseSync();
+export const yarg: {
+  [x: string]: unknown;
+  b: number;
+  l: number;
+  s: boolean;
+  _: (string | number)[];
+  $0: string;
+} = yargs(hideBin(process.argv))
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+    describe: "Multiplication table base",
+  })
+  .option("l", {
+    alias: "limit",
+    type: "number",
+    default: 10,
+    describe: "Multiplication table limit",
+  })
+  .option("s", {
+    alias: "show",
+    type: "boolean",
+    default: false,
+    describe: "Show multiplication table",
+  })
+  .check((argv: any, options: any) => {
+    if (argv.b < 1)
+      throw new Error("Error: The base must be a number greater than 0");
+    return true;
+  })
+  .parseSync();
