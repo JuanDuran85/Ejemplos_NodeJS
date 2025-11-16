8aef6287ad74518c8ef3845f3adbd79fc8c9f96d
JuanDuran85 <duran85juan@gmail.com>
2025-11-16 19:14:01 -0300

    feat: implement domain layer with log entity, datasource and repository abstractions

---
+++ 05-noc/src/domain/entities/log.entity.ts
@@ -0,0 +1,21 @@
+export enum LogSeverityLevel {
+  DEBUG = "debug",
+  INFO = "info",
+  WARN = "warn",
+  ERROR = "error",
+  LOW = "low",
+  MEDIUM = "medium",
+  HIGH = "high",
+}
+
+export class LogEntity {
+  public level: LogSeverityLevel;
+  public message: string;
+  public createdAt: Date;
+
+  constructor(level: LogSeverityLevel, message: string) {
+    this.level = level;
+    this.message = message;
+    this.createdAt = new Date();
+  }
+}
