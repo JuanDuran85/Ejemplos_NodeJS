993741f0802850141ce504c520dc7fc5a3af11eb
JuanDuran85 <duran85juan@gmail.com>
2025-11-17 18:26:49 -0300

    Remove unused email service imports and commented code from server

--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -1,10 +1,8 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
-import { SendEmailWithoutAttachments } from "../domain/use-cases/email/send-email-without-attachment";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
-import { SendEmailWithAttachmentLogs } from "../domain/use-cases/email/send-email-with-attachment-logs";
 
 const fileSystemLogRepository: LogRepositoryImpl = new LogRepositoryImpl(
   new FileSystemDatasource()
@@ -14,25 +12,6 @@ const emailService: EmailService = new EmailService();
 export class ServerApp {
   public static start(): void {
     console.debug("Server Started...");
-
-    /*     new SendEmailWithoutAttachments(
-      fileSystemLogRepository,
-      emailService
-    ).execute({
-      from: "test@gmail.com",
-      to: "test@gmail.com",
-      subject: "Test Email",
-      htmlBody: `
-        <h1>Test Email</h1>
-        <p>This is a test email.</p>
-        <p>Thank you.</p>
-      `,
-    });
-
-    new SendEmailWithAttachmentLogs(
-      fileSystemLogRepository,
-      emailService
-    ).execute(["test@gmail.com"]); */
 
     CronService.createJob("*/10 * * * * *", () => {
       const date: Date = new Date();
