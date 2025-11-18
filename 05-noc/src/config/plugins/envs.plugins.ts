911a785b880f39c2e0446ad24aa83f2e9a98665c
JuanDuran85 <duran85juan@gmail.com>
2025-11-18 15:40:19 -0300

    Add MongoDB configuration and Docker setup
    - Add MongoDB environment variables to envs.plugins.ts
    - Create docker-compose.yml for MongoDB service
    - Update .env.template with MongoDB configuration
    - Add mongo-db and mongo directories to .gitignore

--- 05-noc/src/config/plugins/envs.plugins.ts
+++ 05-noc/src/config/plugins/envs.plugins.ts
@@ -7,4 +7,8 @@ export const envs = {
   EMAIL_KEY: env.get("EMAIL_KEY").required().asString(),
   PROD: env.get("PROD").required().asBool(),
   EMAIL_SERVICE: env.get("EMAIL_SERVICE").required().asString(),
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASSWORD: env.get("MONGO_PASSWORD").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB").required().asString(),
 };
