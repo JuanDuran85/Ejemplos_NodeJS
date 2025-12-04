c84b1657e1e79c8884742c512b9f89c6357645d0
JuanDuran85 <duran85juan@gmail.com>
2025-12-04 15:49:44 -0300

    Add test for GET /api/todos/:id endpoint and improve test setup

--- 07-restweb/test/presentation/todos/router.test.ts
+++ 07-restweb/test/presentation/todos/router.test.ts
@@ -1,4 +1,12 @@
-import { afterAll, beforeAll, describe, expect, it } from "@jest/globals";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from "@jest/globals";
 import request from "supertest";
 import { testServer } from "../../test-server";
 import { prisma } from "../../../src/data";
@@ -6,9 +14,15 @@ import { prisma } from "../../../src/data";
 const todo1 = { task: "Test Task 1", completedAt: null };
 const todo2 = { task: "Test Task 2", completedAt: null };
 
+jest.setTimeout(1_000_000);
+
 describe("Router Test", () => {
   beforeAll(async () => {
     await testServer.start();
+  });
+
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
   });
 
   afterAll(async () => {
@@ -17,7 +31,6 @@ describe("Router Test", () => {
   });
 
   it("Should return TODOs api/todos", async () => {
-    await prisma.todo.deleteMany();
     await prisma.todo.createMany({
       data: [todo1, todo2],
     });
@@ -30,5 +43,22 @@ describe("Router Test", () => {
     expect(body.length).toBe(2);
     expect(body[0].task).toBe(todo1.task);
     expect(body[1].task).toBe(todo2.task);
+  });
+
+  it("should return a TODO api/todos/:id", async () => {
+    const { id, completedAt, task, completed } = await prisma.todo.create({
+      data: todo1,
+    });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${id}`)
+      .set("Content-Type", "application/json")
+      .expect(200);
+    expect(body).toEqual({
+      id,
+      task,
+      completedAt,
+      completed,
+    });
   });
 });
