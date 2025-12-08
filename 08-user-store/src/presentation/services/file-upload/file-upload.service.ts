a2e8197de23c1768dea2eef852394c6d4189d5e1
JuanDuran85 <duran85juan@gmail.com>
2025-12-08 18:11:08 -0300

    Add file upload functionality with express-fileupload middleware

---
+++ 08-user-store/src/presentation/services/file-upload/file-upload.service.ts
@@ -0,0 +1,9 @@
+export class FileUploadService {
+  private checkFolder(folderPath: string) {
+    throw new Error("Method not implemented.");
+  }
+
+  public uploadSingle() {}
+
+  public uploadMultiple() {}
+}
